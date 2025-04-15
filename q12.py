6e29ab7828c679ef918726c4a6a4e69a195874ea
vinniciuxs <vinniciusthe@gmail.com>
2025-04-15 20:33:28 -0300

    Update q12.py

--- q12.py
+++ q12.py
@@ -1,1 +1,8 @@
+#Questão 12
+nota1=float(input("Insira sua primeira nota: "))
+nota2=float(input("insira sua segunda nota: "))
+a=nota1*3.5
+b=nota2*7.5
+media=(a+b)/2
+print("Sua média foi: ", media)
 
