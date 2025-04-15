ad6dfc2e210eefee92976461694a62998386ee6a
vinniciuxs <vinniciusthe@gmail.com>
2025-04-15 20:32:40 -0300

    Update q10.py

--- q10.py
+++ q10.py
@@ -1,5 +1,9 @@
-#Questão 2
-name = 'Vinnicius'
-curso = 'Licenciatura em ccomputação'
-print(name)
-print(curso)
+#Questão 10
+salariobruto=5000
+inss=5000*0.15
+ir=5000*0.10
+salarioliquido=salariobruto-ir-inss
+print(salariobruto)
+print(inss)
+print(ir)
+print(salarioliquido)
